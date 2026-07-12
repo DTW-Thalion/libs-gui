b582dd0bf50664a574ecca237f9331590500419d
Todd White <todd.white@thalion.global>
2026-07-12 11:58:37 -0400

    scratch: NSTableColumn probe

--- Tests/oracle/appkit_probe.m
+++ Tests/oracle/appkit_probe.m
@@ -1,13 +1,6 @@
-/* Apple oracle: exact re-selection rule when removing the selected item
-   from an NSPopUpButtonCell. */
+/* Apple oracle for the NSTableColumn coverage test: defaults, width
+   clamping, the resizable/resizing-mask relationship, and the title. */
 #import <Cocoa/Cocoa.h>
-
-static NSPopUpButtonCell *make(void)
-{
-  NSPopUpButtonCell *c = [[NSPopUpButtonCell alloc] initTextCell: @"" pullsDown: NO];
-  [c addItemsWithTitles: [NSArray arrayWithObjects: @"a", @"b", @"c", @"d", nil]];
-  return c;
-}
 
 int main(void)
 {
@@ -16,33 +9,59 @@ int main(void)
     setvbuf(stdout, NULL, _IONBF, 0);
     [NSApplication sharedApplication];
 
-    /* Remove the selected middle item. */
-    NSPopUpButtonCell *c1 = make();
-    [c1 selectItemAtIndex: 1];        /* b */
-    [c1 removeItemAtIndex: 1];        /* -> a c d */
-    printf("PUB rm selected middle(1): selIndex=%ld selTitle='%s'\n",
-           (long)[c1 indexOfSelectedItem], [[c1 titleOfSelectedItem] UTF8String]);
+    NSTableColumn *c = [[NSTableColumn alloc] initWithIdentifier: @"col1"];
+    printf("TC identifier='%s'\n", [[c identifier] UTF8String]);
+    printf("TC defaults: width=%g minWidth=%g maxWidth=%g resizingMask=%lu\n",
+           [c width], [c minWidth], [c maxWidth], (unsigned long)[c resizingMask]);
+    printf("TC defaults: resizable=%d editable=%d hidden=%d\n",
+           [c isResizable], [c isEditable], [c isHidden]);
+    printf("TC defaults: headerCell=%s dataCell=%s\n",
+           [NSStringFromClass([[c headerCell] class]) UTF8String],
+           [NSStringFromClass([[c dataCell] class]) UTF8String]);
 
-    /* Remove the selected last item. */
-    NSPopUpButtonCell *c2 = make();
-    [c2 selectItemAtIndex: 3];        /* d */
-    [c2 removeItemAtIndex: 3];        /* -> a b c */
-    printf("PUB rm selected last(3): selIndex=%ld selTitle='%s'\n",
-           (long)[c2 indexOfSelectedItem], [[c2 titleOfSelectedItem] UTF8String]);
+    /* Width clamping. */
+    [c setMinWidth: 20];
+    [c setMaxWidth: 200];
+    [c setWidth: 50];
+    printf("TC setWidth 50 -> %g\n", [c width]);
+    [c setWidth: 5];
+    printf("TC setWidth 5 (below min 20) -> %g\n", [c width]);
+    [c setWidth: 500];
+    printf("TC setWidth 500 (above max 200) -> %g\n", [c width]);
 
-    /* Remove a non-selected item below the selection. */
-    NSPopUpButtonCell *c3 = make();
-    [c3 selectItemAtIndex: 2];        /* c */
-    [c3 removeItemAtIndex: 0];        /* -> b c d, selection follows c */
-    printf("PUB rm non-selected below (sel2 rm0): selIndex=%ld selTitle='%s'\n",
-           (long)[c3 indexOfSelectedItem], [[c3 titleOfSelectedItem] UTF8String]);
+    /* setMinWidth / setMaxWidth push the width. */
+    [c setWidth: 100];
+    [c setMinWidth: 150];
+    printf("TC width=100 then setMinWidth 150 -> width=%g\n", [c width]);
+    [c setWidth: 180];
+    [c setMaxWidth: 160];
+    printf("TC width=180 then setMaxWidth 160 -> width=%g\n", [c width]);
 
-    /* Remove the only item. */
-    NSPopUpButtonCell *c4 = [[NSPopUpButtonCell alloc] initTextCell: @"" pullsDown: NO];
-    [c4 addItemWithTitle: @"only"];
-    [c4 removeItemAtIndex: 0];
-    printf("PUB rm only item: numItems=%ld selIndex=%ld\n",
-           (long)[c4 numberOfItems], (long)[c4 indexOfSelectedItem]);
+    /* The resizable / resizing-mask relationship. */
+    NSTableColumn *r = [[NSTableColumn alloc] initWithIdentifier: @"r"];
+    [r setResizingMask: NSTableColumnNoResizing];
+    printf("TC setResizingMask 0 -> isResizable=%d\n", [r isResizable]);
+    [r setResizingMask: NSTableColumnUserResizingMask];
+    printf("TC setResizingMask User -> isResizable=%d\n", [r isResizable]);
+    NSTableColumn *r2 = [[NSTableColumn alloc] initWithIdentifier: @"r2"];
+    [r2 setResizable: NO];
+    printf("TC setResizable NO -> resizingMask=%lu isResizable=%d\n",
+           (unsigned long)[r2 resizingMask], [r2 isResizable]);
+    [r2 setResizable: YES];
+    printf("TC setResizable YES -> resizingMask=%lu isResizable=%d\n",
+           (unsigned long)[r2 resizingMask], [r2 isResizable]);
+
+    /* Title convenience (goes through the header cell). */
+    [c setTitle: @"Name"];
+    printf("TC setTitle -> title='%s' headerCell.stringValue='%s'\n",
+           [[c title] UTF8String], [[[c headerCell] stringValue] UTF8String]);
+
+    /* Simple round-trips. */
+    [c setEditable: NO];
+    [c setHidden: YES];
+    [c setIdentifier: @"other"];
+    printf("TC roundtrip editable=%d hidden=%d identifier='%s'\n",
+           [c isEditable], [c isHidden], [[c identifier] UTF8String]);
 
     printf("DONE\n");
   }
