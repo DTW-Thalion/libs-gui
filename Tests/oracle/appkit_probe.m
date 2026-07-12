0892e67f5b39999a66fb06e284ff88cfdb64d234
Todd White <todd.white@thalion.global>
2026-07-12 11:28:50 -0400

    scratch: popup removal re-selection probe

--- Tests/oracle/appkit_probe.m
+++ Tests/oracle/appkit_probe.m
@@ -1,6 +1,13 @@
-/* Apple oracle for the NSPopUpButtonCell coverage test: defaults, the menu
-   item list, auto-selection, duplicate-title handling and selection. */
+/* Apple oracle: exact re-selection rule when removing the selected item
+   from an NSPopUpButtonCell. */
 #import <Cocoa/Cocoa.h>
+
+static NSPopUpButtonCell *make(void)
+{
+  NSPopUpButtonCell *c = [[NSPopUpButtonCell alloc] initTextCell: @"" pullsDown: NO];
+  [c addItemsWithTitles: [NSArray arrayWithObjects: @"a", @"b", @"c", @"d", nil]];
+  return c;
+}
 
 int main(void)
 {
@@ -9,68 +16,33 @@ int main(void)
     setvbuf(stdout, NULL, _IONBF, 0);
     [NSApplication sharedApplication];
 
-    NSPopUpButtonCell *c = [[NSPopUpButtonCell alloc] initTextCell: @"" pullsDown: NO];
-    printf("PUB defaults: pullsDown=%d autoenables=%d usesItemFromMenu=%d altersState=%d\n",
-           [c pullsDown], [c autoenablesItems], [c usesItemFromMenu], [c altersStateOfSelectedItem]);
-    printf("PUB defaults: preferredEdge=%lu (maxY=%lu) arrowPosition=%ld numItems=%ld selIndex=%ld\n",
-           (unsigned long)[c preferredEdge], (unsigned long)NSMaxYEdge,
-           (long)[c arrowPosition], (long)[c numberOfItems], (long)[c indexOfSelectedItem]);
+    /* Remove the selected middle item. */
+    NSPopUpButtonCell *c1 = make();
+    [c1 selectItemAtIndex: 1];        /* b */
+    [c1 removeItemAtIndex: 1];        /* -> a c d */
+    printf("PUB rm selected middle(1): selIndex=%ld selTitle='%s'\n",
+           (long)[c1 indexOfSelectedItem], [[c1 titleOfSelectedItem] UTF8String]);
 
-    /* Adding the first item auto-selects it. */
-    [c addItemWithTitle: @"alpha"];
-    printf("PUB after add alpha: numItems=%ld selIndex=%ld selTitle='%s'\n",
-           (long)[c numberOfItems], (long)[c indexOfSelectedItem],
-           [[c titleOfSelectedItem] UTF8String]);
-    [c addItemsWithTitles: [NSArray arrayWithObjects: @"beta", @"gamma", nil]];
-    printf("PUB after add 2 more: numItems=%ld selIndex=%ld titles=%s\n",
-           (long)[c numberOfItems], (long)[c indexOfSelectedItem],
-           [[[c itemTitles] componentsJoinedByString: @","] UTF8String]);
+    /* Remove the selected last item. */
+    NSPopUpButtonCell *c2 = make();
+    [c2 selectItemAtIndex: 3];        /* d */
+    [c2 removeItemAtIndex: 3];        /* -> a b c */
+    printf("PUB rm selected last(3): selIndex=%ld selTitle='%s'\n",
+           (long)[c2 indexOfSelectedItem], [[c2 titleOfSelectedItem] UTF8String]);
 
-    /* Duplicate title moves the item; no duplicate is created. */
-    [c addItemWithTitle: @"beta"];
-    printf("PUB after add dup beta: numItems=%ld indexOf beta=%ld\n",
-           (long)[c numberOfItems], (long)[c indexOfItemWithTitle: @"beta"]);
+    /* Remove a non-selected item below the selection. */
+    NSPopUpButtonCell *c3 = make();
+    [c3 selectItemAtIndex: 2];        /* c */
+    [c3 removeItemAtIndex: 0];        /* -> b c d, selection follows c */
+    printf("PUB rm non-selected below (sel2 rm0): selIndex=%ld selTitle='%s'\n",
+           (long)[c3 indexOfSelectedItem], [[c3 titleOfSelectedItem] UTF8String]);
 
-    /* Insert at a position. */
-    [c insertItemWithTitle: @"inserted" atIndex: 1];
-    printf("PUB insert at 1: title@1='%s' numItems=%ld\n",
-           [[c itemTitleAtIndex: 1] UTF8String], (long)[c numberOfItems]);
-
-    /* Queries. */
-    printf("PUB indexOf gamma=%ld itemWithTitle alpha!=nil:%d lastItem title='%s'\n",
-           (long)[c indexOfItemWithTitle: @"gamma"],
-           [c itemWithTitle: @"alpha"] != nil, [[[c lastItem] title] UTF8String]);
-    printf("PUB indexOf missing=%ld\n", (long)[c indexOfItemWithTitle: @"nope"]);
-
-    /* Tag lookup. */
-    [[c itemAtIndex: 2] setTag: 42];
-    printf("PUB indexOfItemWithTag 42=%ld tag99=%ld\n",
-           (long)[c indexOfItemWithTag: 42], (long)[c indexOfItemWithTag: 99]);
-
-    /* Selection. */
-    [c selectItemAtIndex: 2];
-    printf("PUB select 2: selIndex=%ld selTitle='%s'\n",
-           (long)[c indexOfSelectedItem], [[c titleOfSelectedItem] UTF8String]);
-    [c selectItemWithTitle: @"alpha"];
-    printf("PUB selectWithTitle alpha: selIndex=%ld\n", (long)[c indexOfSelectedItem]);
-
-    /* Removal (removing the selected item). */
-    [c selectItemAtIndex: 0];
-    [c removeItemAtIndex: 0];
-    printf("PUB remove selected 0: numItems=%ld selIndex=%ld\n",
-           (long)[c numberOfItems], (long)[c indexOfSelectedItem]);
-    [c removeAllItems];
-    printf("PUB removeAll: numItems=%ld selIndex=%ld\n",
-           (long)[c numberOfItems], (long)[c indexOfSelectedItem]);
-
-    /* Property round-trips. */
-    [c setPullsDown: YES];
-    [c setAutoenablesItems: NO];
-    [c setPreferredEdge: NSMinXEdge];
-    [c setArrowPosition: NSPopUpArrowAtBottom];
-    printf("PUB roundtrip pullsDown=%d autoenables=%d preferredEdge=%lu arrow=%ld\n",
-           [c pullsDown], [c autoenablesItems],
-           (unsigned long)[c preferredEdge], (long)[c arrowPosition]);
+    /* Remove the only item. */
+    NSPopUpButtonCell *c4 = [[NSPopUpButtonCell alloc] initTextCell: @"" pullsDown: NO];
+    [c4 addItemWithTitle: @"only"];
+    [c4 removeItemAtIndex: 0];
+    printf("PUB rm only item: numItems=%ld selIndex=%ld\n",
+           (long)[c4 numberOfItems], (long)[c4 indexOfSelectedItem]);
 
     printf("DONE\n");
   }
