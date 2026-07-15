cdfcc58202bb36e393202108e5e2d71db9758ba5
Todd White <todd.white@thalion.global>
2026-07-15 15:39:49 -0400

    probe: NSPathComponentCell

--- Tests/oracle/appkit_probe.m
+++ Tests/oracle/appkit_probe.m
@@ -1,14 +1,19 @@
-/* Apple oracle for the NSRulerMarker coverage test.  Probes the init defaults
-   (markerLocation, imageOrigin, image, ruler, movable, removable,
-   representedObject), the thicknessRequiredInRuler computation, the plain
-   setter round-trips and the nil-argument exceptions.  Portable so the same
-   file runs under GNUstep for an A/B. */
+/* Apple oracle for the NSPathComponentCell coverage test.  Probes the init
+   defaults (image, URL, title, stringValue), whether setImage:/setURL: keep
+   the same object, whether setURL: changes the title, and the setTitle:
+   round-trip.  Portable so the same file runs under GNUstep for an A/B. */
 #ifdef __APPLE__
 #import <Cocoa/Cocoa.h>
 #else
 #import <AppKit/AppKit.h>
 #endif
 #include <stdio.h>
+
+static const char *
+s(NSString *v)
+{
+  return v == nil ? "nil" : (const char *)[v UTF8String];
+}
 
 int
 main(int argc, const char **argv)
@@ -18,57 +23,23 @@ main(int argc, const char **argv)
   {
     [NSApplication sharedApplication];
 
-    NSScrollView *sv =
-        [[NSScrollView alloc] initWithFrame: NSMakeRect(0, 0, 200, 200)];
-    NSRulerView *rv = [[NSRulerView alloc] initWithScrollView: sv
-                                                  orientation: NSHorizontalRuler];
-    NSImage *img = [[NSImage alloc] initWithSize: NSMakeSize(20, 16)];
+    NSPathComponentCell *c = [[NSPathComponentCell alloc] init];
+    printf("INIT image=%s url=%s title=[%s] stringValue=[%s]\n",
+           [c image] == nil ? "nil" : "set",
+           [c URL] == nil ? "nil" : "set",
+           s([c title]), s([c stringValue]));
 
-    NSRulerMarker *m = [[NSRulerMarker alloc] initWithRulerView: rv
-                                                 markerLocation: 100.0
-                                                          image: img
-                                                    imageOrigin: NSMakePoint(3, 4)];
-    printf("INIT loc=%g imgOrigin=%gx%g imageSame=%d rulerSame=%d movable=%d removable=%d repObj=%s\n",
-           [m markerLocation], [m imageOrigin].x, [m imageOrigin].y,
-           [m image] == img, [m ruler] == rv, [m isMovable], [m isRemovable],
-           [m representedObject] == nil ? "nil" : "set");
-    printf("INIT thickness=%g\n", [m thicknessRequiredInRuler]);
+    NSImage *img = [[NSImage alloc] initWithSize: NSMakeSize(16, 16)];
+    [c setImage: img];
+    printf("IMG same=%d nonnil=%d\n", [c image] == img, [c image] != nil);
 
-    [m setMarkerLocation: 200.0];
-    [m setImageOrigin: NSMakePoint(5, 6)];
-    [m setMovable: NO];
-    [m setRemovable: YES];
-    [m setRepresentedObject: @"obj"];
-    printf("SET loc=%g imgOrigin=%gx%g movable=%d removable=%d repObj=%s\n",
-           [m markerLocation], [m imageOrigin].x, [m imageOrigin].y,
-           [m isMovable], [m isRemovable],
-           [m representedObject] == nil ? "nil"
-             : [[[m representedObject] description] UTF8String]);
+    NSURL *url = [NSURL fileURLWithPath: @"/tmp/foo"];
+    [c setURL: url];
+    printf("URL same=%d equal=%d title=[%s] stringValue=[%s]\n",
+           [c URL] == url, [[c URL] isEqual: url], s([c title]), s([c stringValue]));
 
-    @try
-      {
-        [[NSRulerMarker alloc] initWithRulerView: nil
-                                  markerLocation: 0.0
-                                           image: img
-                                     imageOrigin: NSZeroPoint];
-        printf("EXC nilRuler no-raise\n");
-      }
-    @catch (NSException *e)
-      {
-        printf("EXC nilRuler raised %s\n", [[e name] UTF8String]);
-      }
-    @try
-      {
-        [[NSRulerMarker alloc] initWithRulerView: rv
-                                  markerLocation: 0.0
-                                           image: nil
-                                     imageOrigin: NSZeroPoint];
-        printf("EXC nilImage no-raise\n");
-      }
-    @catch (NSException *e)
-      {
-        printf("EXC nilImage raised %s\n", [[e name] UTF8String]);
-      }
+    [c setTitle: @"MyTitle"];
+    printf("TITLE title=[%s]\n", s([c title]));
   }
   return 0;
 }
